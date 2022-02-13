256af47082d318d05bcb93a1b6e163e2abeb942b
J2mF <mp4-12cs5@hotmail.com>
2022-02-13 20:56:27 +0200

    Update Coach Requests
    Change Log:
    - Show requests for their coach only

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,8 +1,9 @@
 export default {
-  requests(state) {
-    return state.requests;
+  requests(state, getters, rootState, rootGetters) {
+    const userId = rootGetters.userId;
+    return state.requests.filter(value => value.coachId === userId);
   },
-  hasRequests(state) {
-    return state.requests && state.requests.length > 0;
+  hasRequests(state, getters) {
+    return getters.requests && getters.requests.length > 0;
   }
 }
